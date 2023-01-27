a7a18cf30a8974f8fce95c0ec1a58c0f74eee26e
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2023-01-27 21:36:56 +0000

    blog post styling done

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 
 import "../styles/globals.css";
+import { polyfill } from 'interweave-ssr';
+
+polyfill();
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,39 +1,43 @@
-import { type NextPage } from "next";
-import { type GetServerSideProps } from 'next'
-import BlogDisplay from "../components/BlogDisplay";
-
-
+// import { type NextPage } from "next";
+import { type GetServerSideProps } from "next";
+// import BlogDisplay from "../components/BlogDisplay";
+// import Link from "next/link";
+import Image from "next/image";
+import { Markup } from "interweave";
 
 type BlogPostProps = {
-  _id: string,
-  title: string,
-  description: string,
-  image: string
-}
-
-function Home({posts}:{posts: BlogPostProps[]} ) {
-  console.log("posts inside home", posts)
-  return (
-   <div className="flex flex-col items-center">
-   
-   {posts.map((post, index: number) => 
-    <div className="flex flex-row" key={index}>
-      <BlogDisplay post={post} />
-    </div>
-      
-   )}
-   </div>
-  );
+  _id: string;
+  title: string;
+  description: string;
+  image: string;
 };
 
+function Home({ posts }: { posts: BlogPostProps[] }) {
+  // console.log("posts inside home", posts)
+  return (
+    <div className="m-auto my-10 flex flex-col items-center">
+      {posts.map((post, index: number) => (
+        <div className="flex even:flex-row odd:flex-row-reverse w-3/5 h-96 border mt-10 bg-[#F2F7FF] justify-between odd:justify-end p-4 rounded" key={index}>
+          
+            <div className="text-gray-700 p-4 text-base ">
+              <h2 className="font-bold mb-4 text-xl">{post.title.toUpperCase()}</h2>
+              <Markup content={`${post.description.substring(0, 100)}...`} />
+            </div>
+            <Image src={post.image} alt="Blog image" width="475" height="100" />
+          
+        </div>
+      ))}
+    </div>
+  );
+}
 
 export const getServerSideProps: GetServerSideProps = async () => {
   const response = await fetch(process.env.BACKEND_URL);
   const posts: BlogPostProps[] = await response.json();
-  
+
   return {
     props: { posts: posts },
-  }
+  };
 };
 
 export default Home;
