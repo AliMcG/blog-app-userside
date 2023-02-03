e77d22a7f07b17a73c69fd9f2635059a48d396d1
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2023-02-03 21:34:13 +0000

    type error on svg added webpack config to next.config and .babelrc file

--- src/@types/assets/globals.d.ts
+++ src/@types/assets/globals.d.ts
@@ -1,5 +1,7 @@
 
-declare module "\*.svg" {  import React = require("react");  const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;  export default ReactComponent;}declare module "\*.jpg" {
+declare module "\*.svg" {  import React = require("react");  const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;  export default ReactComponent;}
+
+declare module "\*.jpg" {
   const content: string;
   export default content;
 }
