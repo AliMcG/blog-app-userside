27f2a28f29cf78d0493abb086fe74bc973fb30da
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2023-02-07 20:24:19 +0000

    Layout component and basic pages for about and contact

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,43 +1,40 @@
-
 import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/router";
-
 
 import titleImage from "../../public/harry-new-header.png";
 
 function Navbar() {
   const router = useRouter();
-  
 
   return (
     <>
-      <header className="md:w-auto w-4/5">
-        <Image
-          src={titleImage}
-          alt="Harry Duncton"
-          width="700"
-          height="50"
-        />
-      </header>
+      <div className="m-auto my-10 flex flex-col items-center">
+        <header className="w-4/5 md:w-auto">
+          <Image src={titleImage} alt="Harry Duncton" width="700" height="50" />
+        </header>
 
-      <nav className="flex md:w-2/3 w-4/5 mt-8 justify-evenly text-gray-700 font-sans font-semibold ">
-        <Link className={router.pathname == "/" ? "text-[#073D93]" : ""} href="/">
-          <h1 className="hover:text-[#067A75]" >HOME</h1>
-        </Link>{" "}
-        <Link
-          className={router.pathname == "/about" ? "text-[#073D93]" : ""}
-          href="/about"
-        >
-          <h1 className="hover:text-[#067A75]" >ABOUT</h1>
-        </Link>{" "}
-        <Link
-          className={router.pathname == "/contact" ? "text-[#073D93]" : ""}
-          href="/contact"
-        >
-          <h1 className="hover:text-[#067A75]" >CONTACT</h1>
-        </Link>{" "}
-      </nav>
+        <nav className="mt-8 flex w-4/5 justify-evenly font-sans font-semibold text-gray-700 md:w-2/3 ">
+          <Link
+            className={router.pathname == "/" ? "text-[#073D93]" : ""}
+            href="/"
+          >
+            <h1 className="hover:text-[#067A75]">HOME</h1>
+          </Link>{" "}
+          <Link
+            className={router.pathname == "/about" ? "text-[#073D93]" : ""}
+            href="/about"
+          >
+            <h1 className="hover:text-[#067A75]">ABOUT</h1>
+          </Link>{" "}
+          <Link
+            className={router.pathname == "/contact" ? "text-[#073D93]" : ""}
+            href="/contact"
+          >
+            <h1 className="hover:text-[#067A75]">CONTACT</h1>
+          </Link>{" "}
+        </nav>
+      </div>
     </>
   );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,29 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 import { Montserrat, Frank_Ruhl_Libre } from "@next/font/google";
 import "../styles/globals.css";
+import Layout from "../components/Layout";
 import { polyfill } from "interweave-ssr";
-//  Polyfill function here to manage a hydration error caused by <Markup> component 
+//  Polyfill function here to manage a hydration error caused by <Markup> component
 polyfill();
 
 // To import and add custom fonts to next.js
 const montserrat = Montserrat({
   subsets: ["latin"],
-  variable: '--font-monts',
+  variable: "--font-monts",
 });
 
 const frank = Frank_Ruhl_Libre({
-  subsets: ['latin'],
-  variable: '--font-frank'
-})
+  subsets: ["latin"],
+  variable: "--font-frank",
+});
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <main className={`${montserrat.variable} ${frank.variable} `}>
-      <Component {...pageProps} />
-    </main>
+    <Layout>
+      <main className={`${montserrat.variable} ${frank.variable} `}>
+        <Component {...pageProps} />
+      </main>
+    </Layout>
   );
 };
 
