8e211470e450552c84d82b26f1f9fdc59e947088
alistairmcgill <mcgill.alistair@yahoo.co.uk>
2023-02-07 21:08:37 +0000

    dynamic page csss layout and Links from index work

--- src/pages/[id].tsx
+++ src/pages/[id].tsx
@@ -2,7 +2,6 @@ import { type GetServerSideProps } from "next";
 import Image from "next/image";
 import { Markup } from "interweave";
 
-
 // Defines the type for the blog posts used in this app
 type BlogPostProps = {
   _id: string;
@@ -11,45 +10,43 @@ type BlogPostProps = {
   image: string;
 };
 
-function PostDetails( {post}: {post: BlogPostProps} ) {
-  console.log("a post", post)
+function PostDetails({ post }: { post: BlogPostProps }) {
+  console.log("a post", post);
   return (
     <div className="m-auto my-10 flex flex-col items-center">
-    
-        <div
-          className="mt-10 flex h-4/5 md:w-3/5 mx-4 flex-col-reverse lg:items-start items-center rounded border bg-custom-blue p-4 lg:odd:flex-row-reverse  lg:even:flex-row lg:odd:justify-end"
-         
-        >
-          <div className="p-4 text-base text-gray-700 font-sans">
-            <h2 className="mb-4 text-xl font-bold font-mono">
-              {post.title.toUpperCase()}
-            </h2>
-            <Markup content={post.description} />
-          </div>
-          <div className="relative h-80 lg:w-3/5 w-11/12">
-            <Image src={post.image} alt="Blog image" sizes="" fill className="rounded"/>
-          </div>
+      <div className="mx-4 mt-10 flex h-4/5 flex-col-reverse items-center rounded border bg-custom-blue p-4 md:w-3/5 ">
+        <div className="p-4 font-sans text-base text-gray-700">
+          <h2 className="mb-4 font-mono text-xl font-bold">
+            {post.title.toUpperCase()}
+          </h2>
+          <Markup content={post.description} />
         </div>
-     
+        <div className="relative h-80  w-11/12">
+          <Image
+            src={post.image}
+            alt="Blog image"
+            sizes=""
+            fill
+            className="rounded"
+          />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 // A type safe funstion to fetch data
-export async function http<T>(
-  request: RequestInfo
-): Promise<T> {
+export async function http<T>(request: RequestInfo): Promise<T> {
   const response = await fetch(request);
-  const body = await response.json() as T
+  const body = (await response.json()) as T;
   return body;
 }
 
-
 // Type safe GetServerSideProps
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const id = context.params?.id as string
-  const url = process.env.BACKEND_URL as string
-  const data = await http<BlogPostProps[]>(`${url}/${id}`)
+  const id = context.params?.id as string;
+  const url = process.env.BACKEND_URL as string;
+  const data = await http<BlogPostProps[]>(`${url}/${id}`);
   return {
     props: { post: data },
   };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,6 @@ import { type GetServerSideProps } from "next";
 import Image from "next/image";
 import Link from "next/link";
 import { Markup } from "interweave";
-// import Navbar from "../components/NavBar";
-// import Footer from "../components/Footer";
 
 // Defines the type for the blog posts used in this app
 type BlogPostProps = {
@@ -13,45 +11,49 @@ type BlogPostProps = {
   image: string;
 };
 
-function Home( {posts}: {posts: BlogPostProps[]} ) {
+function Home({ posts }: { posts: BlogPostProps[] }) {
   return (
     <div className="m-auto my-10 flex flex-col items-center">
-      {/* <Navbar /> */}
       {posts.map((post, index: number) => (
         <div
-          className="mt-10 flex h-4/5 md:w-3/5 mx-4 flex-col-reverse lg:items-start items-center rounded border bg-custom-blue p-4 lg:odd:flex-row-reverse  lg:even:flex-row lg:odd:justify-end"
+          className="mx-4 mt-10 flex h-4/5 flex-col-reverse items-center rounded border bg-custom-blue p-4 md:w-3/5 lg:items-start lg:odd:flex-row-reverse  lg:odd:justify-end lg:even:flex-row"
           key={index}
         >
           <Link href={`/${post._id}`}>
+            <div className="p-4 font-sans text-base text-gray-700 hover:text-[#067A75]">
+              <h2 className="mb-4 font-mono text-xl font-bold hover:text-[#067A75]">
+                {post.title.toUpperCase()}
+              </h2>
+              <Markup content={`${post.description.substring(0, 100)}...`} />
+            </div>
           </Link>
-          <div className="p-4 text-base text-gray-700 font-sans">
-            <h2 className="mb-4 text-xl font-bold font-mono">
-              {post.title.toUpperCase()}
-            </h2>
-            <Markup content={`${post.description.substring(0, 100)}...`} />
-          </div>
-          <div className="relative h-80 lg:w-3/5 w-11/12">
-            <Image src={post.image} alt="Blog image" sizes="" fill className="rounded"/>
+          <div className="relative h-80 w-11/12 lg:w-3/5">
+            <Link href={`/${post._id}`}>
+              <Image
+                src={post.image}
+                alt="Blog image"
+                sizes=""
+                fill
+                className="rounded"
+              />
+            </Link>
           </div>
         </div>
       ))}
-      {/* <Footer /> */}
     </div>
-  )
+  );
 }
 
 // A type safe funstion to fetch data
-export async function http<T>(
-  request: RequestInfo
-): Promise<T> {
+export async function http<T>(request: RequestInfo): Promise<T> {
   const response = await fetch(request);
-  const body = await response.json() as T
+  const body = (await response.json()) as T;
   return body;
 }
 
 // Type safe GetServerSideProps
 export const getServerSideProps: GetServerSideProps = async () => {
-  const data = await http<BlogPostProps[]>(process.env.BACKEND_URL as string)
+  const data = await http<BlogPostProps[]>(process.env.BACKEND_URL as string);
   return {
     props: { posts: data },
   };
